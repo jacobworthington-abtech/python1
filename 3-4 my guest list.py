470ed0b0aa40b74ea78469997f4ef443475e4292
jacobworthington-abtech <09jacobw24@gmail.com>
2025-01-28 12:28:32 -0500

    Add files via upload

---
+++ 3-4 my guest list.py
@@ -0,0 +1,6 @@
+# Initial guest list
+guest_list = ["My Grandfather", "Albert Einstein", "Billy Graham"]
+
+# Print invitation messages for the initial list
+for guest in guest_list:
+    print(f"Dear {guest}, you are cordially invited to dinner.")
