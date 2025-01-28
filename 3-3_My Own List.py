84dc47955325c9c96864f4915a830dba7d9fed72
jacobworthington-abtech <09jacobw24@gmail.com>
2025-01-28 11:42:59 -0500

    Add files via upload

---
+++ 3-3_My Own List.py
@@ -0,0 +1,10 @@
+# Create a list to store key motorcycle-related statements
+motorcycle_statements = [
+    "I've been on a professional race track.",
+    "I've a picture of me dragging my elbow on that track.",
+    "I've seen 165 mph on that track."
+]
+
+# Print each statement from the list
+for statement in motorcycle_statements:
+    print(statement)
